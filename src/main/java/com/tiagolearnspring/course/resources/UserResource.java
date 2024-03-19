23bf77d3ddf472e304e0e971076530797ee9feac
Tiago Gomes <tidgomes@gmail.com>
2024-03-19 10:50:14 -0300

    create route to find user by id

--- src/main/java/com/tiagolearnspring/course/resources/UserResource.java
+++ src/main/java/com/tiagolearnspring/course/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.tiagolearnspring.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tiagolearnspring.course.entities.User;
+import com.tiagolearnspring.course.services.UsersServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UsersServices service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Mano", "mano@mail.com", "pass123");
-		return ResponseEntity.ok(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok(user);
 	}
 
 }
---
+++ src/main/java/com/tiagolearnspring/course/services/UsersServices.java
@@ -0,0 +1,25 @@
+package com.tiagolearnspring.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagolearnspring.course.entities.User;
+import com.tiagolearnspring.course.repositories.UserRepository;
+
+@Service
+public class UsersServices {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		return repository.findById(id).get();
+	}
+}
