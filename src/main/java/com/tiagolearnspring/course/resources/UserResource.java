0822d48157fb87da0963d3ac77da691394070a63
Tiago Gomes <tidgomes@gmail.com>
2024-03-19 09:14:32 -0300

    create User entity and resource

---
+++ src/main/java/com/tiagolearnspring/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.tiagolearnspring.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tiagolearnspring.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Mano", "mano@mail.com", "pass123");
+		return ResponseEntity.ok(u);
+	}
+
+}
